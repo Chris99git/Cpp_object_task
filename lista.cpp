16eed892a7b25b862eeb64c8bf9eed49fc17cd39
Chris99git <krzysztofligas7@gmail.com>
2022-04-05 16:56:59 +0200

    Create lista.cpp

---
+++ lista.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+struct slistEl                // typ elementu listy jednokierunkowej
+{
+  slistEl * next;
+  int data;
+};
+
+int main( )
+{
+  slistEl * L;                // wskaŸnik pocz¹tku listy
+  slistEl * e, * p;           // wskaŸniki elementów listy
+  int n=6;                      // liczba elementów do posortowania
+  int v;                      // wartoœæ elementu
+
+  L = new slistEl;            // tworzymy pierwszego stra¿nika
+  L->next = new slistEl;      // tworzymy drugiego stra¿nika
+  L->next->next = NULL;       // drugi stra¿nik jest ostatni na liœcie
+  L->next->data = 2147483647; // wartoœæ drugiego stra¿nika
+
+//  cin >> n;                   // odczytujemy liczbê elementów
+
+  for( int i = 0; i < n; i++ )
+  {
+    cin >> v;                 // czytamy element
+                              // szukamy miejsca wstawienia
+    for( p = L; v > p->next->data; p = p->next ) ;
+
+    e = new slistEl;          // tworzymy nowy element
+
+    e->data = v;              // inicjujemy element
+    e->next = p->next;
+
+    p->next = e;              // element wstawiamy do listy L
+
+  }
+                              // listê przesy³amy na wyjœcie
+  for( p = L->next; p->next; p = p->next ) cout << p->data << " ";
+
+  cout << endl;
+
+  while( L )                  // usuwamy listê z pamiêci
+  {
+    e = L;
+    L = L->next;
+    delete e;
+  }
+
+  return 0;
+}
